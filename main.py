bc43d641d85d5dd5e10200f42e53d797ff02b131
ThisisANOM <143174378+ThisisANOM@users.noreply.github.com>
2024-06-20 15:58:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ class AutoClicker:
         window.activate()
 
         with mss.mss() as sct:
-            grave_key_code = 41
+            V_key_code = 86
             keyboard.add_hotkey(grave_key_code, self.toggle_script)
 
             while True:
@@ -212,7 +212,7 @@ if __name__ == "__main__":
             logger.log("Invalid value")
     is_continue = answer
 
-    logger.log('After starting the mini-game, press the "`" key on the keyboard')
+    logger.log('After starting the mini-game, press the "v" key on the keyboard')
     target_colors_hex = ["#c9e100", "#bae70e"]
     nearby_colors_hex = ["#abff61", "#87ff27"]
     auto_clicker = AutoClicker("TelegramDesktop", target_colors_hex, nearby_colors_hex, logger, percentages=percentages,
